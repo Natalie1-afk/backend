d429921d45b5d70b05aec50a8e553197bbf480f7
Nataliecd <recmig4s@gmail.com>
2026-03-25 22:26:21 -0300

    nuevo endpoint test

--- server.js
+++ server.js
@@ -60,3 +60,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+app.get("/test", (req, res) => {
+  res.send("FUNCIONA TEST OK");
+});
+app.get("/test", (req, res) => {
+  res.send("FUNCIONA TEST OK");
+});
